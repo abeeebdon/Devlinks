c9cab2cd78dafa85ae8a3bbf7c6ffe779a9bb9b7
abeebdon <abeebdon@gmail.com>
2024-12-22 10:27:44 +0100

    AD

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 function loading() {
   return (
     <div className="text-[20px] w-full min-h-screen flex items-center justify-center">
-      Loading....
+      <div className="text-center ">
+        <h2 className="text-purple font-bold text-2xl">Devlinks</h2>
+        <p className="text-gray">Especially for Developers</p>
+        <p className="mt-10">Loading....</p>
+      </div>
     </div>
   )
 }
