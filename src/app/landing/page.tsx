7d15b44138f50b6275634de91430962c9f3cff1e
abeebdon <abeebdon@gmail.com>
2024-12-22 10:07:31 +0100

    landing page

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,30 +1,34 @@
-import {
-  Faq as FAQ,
-  faqs,
-  objectives,
-  Review as Rev,
-  reviews,
-} from '@/components/data'
+import { faqs, objectives, Review as Rev, reviews } from '@/components/data'
 import Image from 'next/image'
 import Link from 'next/link'
-import { FaAngleDown, FaQuoteLeft } from 'react-icons/fa'
+import { FaQuoteLeft } from 'react-icons/fa'
 import { IoIosStar } from 'react-icons/io'
 import FAqs from './FAqs'
-import { FaPhoneVolume } from 'react-icons/fa6'
 import { CiMail, CiPhone } from 'react-icons/ci'
 
 const page = () => {
   return (
     <section className="  bg-lpurple">
       {/* Header  */}
-      <section className="bg-lgray fixed top-0 left-0 right-0 w-full flex justify-center">
+      <section className="bg-purple fixed top-0 left-0 right-0 w-full flex justify-center">
         <header className=" p-4 w-full flex items-center justify-between min-h-[13vh] max-w-[1400px]">
           <div className="flex items-center gap-2">
-            <Image src="/images/logo.svg" width={32} height={32} alt="Logo" />
+            <Image src="/images/logo2.svg" width={32} height={32} alt="Logo" />
 
-            <h2 className="text-xl">devlinks</h2>
+            <h2 className="text-xl text-white">devlinks</h2>
           </div>
-          <div className="flex items-center gap-4 ">
+          <div className="flex items-center text-white gap-4 lg:gap-6">
+            <p>Features </p>
+            <p>Pricing</p>
+            <p>Contact us </p>
+            <Link
+              href="/signup"
+              className="border border-white p-3 text-sm text-white rounded-md  hover:text-lpurple"
+            >
+              Get Started
+            </Link>
+          </div>
+          {/* <div className="flex items-center gap-4 ">
             <Link
               href="/signup"
               className="bg-purple p-3 text-sm text-white rounded-md hover:bg-lpurple hover:text-purple"
@@ -37,17 +41,15 @@ const page = () => {
             >
               Login
             </Link>
-          </div>
+          </div> */}
         </header>
       </section>
       {/* What is Devlinks  */}
-      <article className="mt-[10vh] py-10 flex justify-center">
+      <article className="mt-[10vh] py-10 flex bg-purple justify-center">
         <section className="mt-10 p-4 flex flex-col md:flex-row gap-[7%] justify-center w-full max-w-[1400px]">
           <div className="basis-1/2 max-w-[500px] w-full">
-            <h2 className="text-center text-xl text-purple font-bold">
-              What is Devlinks
-            </h2>
-            <p className=" mt-4">
+            <h2 className=" text-2xl text-white font-bold">What is Devlinks</h2>
+            <p className="text-white my-4">
               Devlinks is an innovative organization dedicated to simplifying
               the way developers share and manage their personal and
               professional links. Recognizing the growing need for a streamlined
@@ -56,12 +58,14 @@ const page = () => {
               resources into a single, customizable page. <br />
               Devlinks ensures that developers spend less time sharing links and
               more time focusing on what truly matters: building and innovating.
-              Beyond just sharing links, Devlinks offers advanced organizational
-              tools that make managing digital assets effortless. The platform
-              includes features such as categorization, analytics, and
-              integration with popular developer tools, ensuring that users can
-              keep track of their most visited or clicked links.
+              Beyond just sharing links.
             </p>
+            <Link
+              href="/signup"
+              className="mt-6 bg-green-600 hover:bg-green-400 text-white border-none p-3 text-lg  rounded-md "
+            >
+              Get Started
+            </Link>
           </div>
           <div className="basis-1/2 h-[300px] ">
             <Image
@@ -75,11 +79,20 @@ const page = () => {
         </section>
       </article>
 
+      <article className="flex bg-lgray justify-center ">
+        <section className="w-full max-w-[1400px] text-gray text-center py-[100px]">
+          <h2 className="text-2xl font-semibold ">Devlinks</h2>
+          <p>Especially for Developers</p>
+        </section>
+      </article>
+
       {/* Our objectives  */}
       <article className="flex justify-center bg-lgray py-20">
         <section className="mt-10 p-4 w-full max-w-[1400px]">
-          <h2 className="text-xl font-semibold text-purple">Our Objectives</h2>
-          <article className="mt-4 flex flex-col md:flex-row items-center gap-4 flex-wrap lg:flex-nowrap">
+          <h2 className="text-2xl font-semibold text-purple text-center">
+            Our Objectives
+          </h2>
+          <article className="mt-8 flex flex-col md:flex-row items-center gap-4 flex-wrap lg:flex-nowrap">
             {objectives.map((obj, index) => {
               return (
                 <div
@@ -96,10 +109,10 @@ const page = () => {
       </article>
 
       {/* Reviews  */}
-      <article className="flex justify-center">
-        <section className="mt-10 p-4 w-full max-w-[1400px]">
-          <h2 className="text-xl font-semibold text-purple">Reviews </h2>
-          <p className="text-gray">
+      <article className="py-[100px] flex justify-center">
+        <section className="mt-10 p-4 w-full max-w-[1400px] text-center">
+          <h2 className="text-2xl font-semibold text-purple">Reviews </h2>
+          <p className="text-gray text-xl">
             Here what our esteemed users are saying about us
           </p>
           <article className="mt-6 flex flex-col md:flex-row items-center gap-4 justify-center flex-wrap lg:flex-nowrap">
@@ -112,8 +125,8 @@ const page = () => {
 
       {/* FaQs  */}
       <article className="flex justify-center">
-        <section className="mt-10 p-4 w-full max-w-[1400px]">
-          <h2 className="text-xl font-semibold text-purple">
+        <section className="mt-20 p-4 w-full max-w-[1400px]">
+          <h2 className="text-2xl font-semibold text-purple text-center">
             Frequently Asked Questions (FAQs)
           </h2>
           <article className="mt-4">
@@ -123,12 +136,16 @@ const page = () => {
           </article>
         </section>
       </article>
-      <footer className="mt-10 min-h-[10vh] p-4 flex justify-center bg-purple">
+      <footer className="relative mt-20 min-h-[15vh] p-4 flex justify-center bg-purple">
         <div className="flex text-white justify-between max-w-[1400px] w-full">
           <div className="flex items-center gap-4 text-white">
-            <Image src="/images/logo.svg" width={32} height={32} alt="Logo" />
-            <p>devlinks</p>
+            <Image src="/images/logo2.svg" width={24} height={24} alt="Logo" />
+
+            <h2 className="text-lg">devlinks</h2>
           </div>
+          <p className="text-lgray opacity-10 absolute text-6xl top-1/3 left-0 right-0 text-center m-auto">
+            Especially for Developers
+          </p>
 
           {/* Contact us  */}
           <div>
@@ -154,9 +171,9 @@ export default page
 
 const Review = ({ author, text, rating }: Rev) => {
   return (
-    <div className="flex-col bg-lgray shadow p-3 rounded-md max-w-[350px]">
-      <h3>{author}</h3>
-      <p className="text-sm my-2">{text}</p>
+    <div className="flex-col text-left bg-lgray shadow p-3 rounded-md max-w-[350px]">
+      <h3 className="text-lg text-gray">{author}</h3>
+      <p className=" my-2 text-dgrap">{text}</p>
       <Rating rating={rating} />
     </div>
   )
