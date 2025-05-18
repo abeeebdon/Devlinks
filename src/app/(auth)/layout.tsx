009d5ebfd582f32f8e2ad84b07638917b7d857a0
Abeeb Maroof <abeebdon@gmail.com>
2025-05-18 07:54:48 +0100

    feat-landing-page:UI restructuring

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import React from 'react'
-import { ReactNode } from 'react'
+import Image from "next/image";
+import React from "react";
+import { ReactNode } from "react";
 
 type AuthLayoutProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
   return (
-    <section className="min-h-screen pt-4 flex items-center justify-center bg-lgray">
-      <article className="w-full max-w-[476px] p-[32px] xs:p-0">
+    <section className="min-h-screen flex items-center justify-center bg-lgray">
+      <article className="w-full max-w-[476px] px-[32px] xs:p-0">
         <header className="w-full flex items-center justify-center mb-[30px] gap-4 text-center">
           <Image src="/images/logo.svg" alt="logo" width={40} height={40} />
           <Image
@@ -22,7 +22,7 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
         <main>{children}</main>
       </article>
     </section>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
--- src/app/landing/LandingHeader.tsx
+++ src/app/landing/LandingHeader.tsx
@@ -1,36 +1,53 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const LandingHeader = () => {
-  const [showSidebar, setShowSideBar] = useState<boolean>(false)
+  const navigation = ["about", "features", "testimonials"];
+  const [showSidebar, setShowSideBar] = useState<boolean>(false);
   return (
-    <header className="p-4 w-full flex items-center justify-between h-[80px] max-w-[1400px]">
+    <header className="p-4 md:px-6 w-full flex items-center justify-between h-[80px] max-w-[1400px]">
       <div className="flex items-center gap-2">
         <Image src="/images/logo2.svg" width={32} height={32} alt="Logo" />
 
         <h2 className="text-xl text-white">devlinks</h2>
       </div>
       <div className="flex flex-row-reverse md:flex-row items-center text-white gap-4 lg:gap-6">
-        <div className=" items-center gap-4 lg:gap-6 hidden md:flex">
-          <p>Features </p>
-          <p>Pricing</p>
-          <p>Contact us </p>
-        </div>
+        <nav className="hidden md:flex gap-6">
+          {navigation.map((nav, i) => {
+            return (
+              <a
+                href={`#${nav}`}
+                key={i}
+                className="capitalize hover:text-indigo-600"
+              >
+                {nav}
+              </a>
+            );
+          })}
+        </nav>
         <div className="md:hidden">
           <FaBars size={30} onClick={() => setShowSideBar(!showSidebar)} />
         </div>
         {showSidebar && (
-          <div className=" absolute bg-gray top-0 p-4  bottom-0 min-h-screen right-0 w-[60%] ">
+          <div className=" absolute bg-gray top-0 p-4  bottom-0 min-h-screen right-0 w-[60%] max-w-[228px] ">
             <div className="flex justify-end">
               <FaTimes size={30} onClick={() => setShowSideBar(false)} />
             </div>
-            <div className="flex-col gap-8 text-lg mt-[5vh]">
-              <p>Features </p>
-              <p>Pricing</p>
-              <p>Contact us </p>
+            <div className="flex-col flex gap-8 text-lg mt-[5vh]">
+              {navigation.map((nav, i) => {
+                return (
+                  <a
+                    href={`#${nav}`}
+                    key={i}
+                    className="capitalize hover:text-indigo-600"
+                  >
+                    {nav}
+                  </a>
+                );
+              })}
               <Link href="/signup">Get Started</Link>
             </div>
           </div>
@@ -44,7 +61,7 @@ const LandingHeader = () => {
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default LandingHeader
+export default LandingHeader;
